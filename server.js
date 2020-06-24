ee2d0744e02c9d69ded67fa4bbf4719e5481b017
James Echmalian <ech@ech.net>
2020-06-24 18:04:17 -0400

    Initial song database.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const socketIO = require('socket.io');
+const {MongoClient} = require("mongodb");
 
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/notion"
 const INDEX = '/index.html';
 
 const server = express()
@@ -16,6 +18,9 @@ const server = express()
   })
   .get("/app/:gameId", (req, res) => {
     res.render("app", { gameId: req.params.gameId })
+  })
+  .get("/song", (req, res) => {
+    get_random_song_title(res)
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
@@ -40,3 +45,32 @@ io.on("connection", (socket) => {
 
 const crypto = require("crypto");
 const createNewGame = () => crypto.randomBytes(12).toString("hex");
+
+
+const get_random_song_title = (res) => {
+  MongoClient.connect(MONGODB_URI, {useUnifiedTopology: true}, (err, db) => {
+    if (err) {
+      res.status(500).send("connect:" + err.toString())
+    }
+    else {
+      db.db("notion").collection("songs").estimatedDocumentCount((err, count) => {
+        if (err) {
+          res.status(500).send("document count:" + err.toString())
+          db.close();
+        }
+        else {
+          const ix = Math.floor(Math.random() * count);
+          db.db("notion").collection("songs").find({}).toArray((err, result) => {
+            if (err) {
+              res.status(500).send("find: " + err.toString())
+            }
+            else {
+              res.send(result[ix].title);
+            }
+            db.close();
+          })
+        }
+      })
+    }
+  })
+}
